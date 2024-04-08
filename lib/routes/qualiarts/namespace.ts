0f98e1d06c9544da74052f8f261e3c70987788f7
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2024-04-08 19:18:23 +0900

    feat(route): qualiarts engineer blog feed

---
+++ lib/routes/qualiarts/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'QualiArts Tech Blog',
+    url: 'technote.qualiarts.jp',
+    description: `
+:::tip
+QualiArts engineer blog feed.
+:::`,
+};
