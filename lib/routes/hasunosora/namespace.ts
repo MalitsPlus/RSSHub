bc66f6df14712f52c91cc900ca54f51992845b6c
MalitsPlus <50281195+MalitsPlus@users.noreply.github.com>
2024-04-19 14:09:56 +0900

    feat(route): hasunosora news

---
+++ lib/routes/hasunosora/namespace.ts
@@ -0,0 +1,10 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: '蓮ノ空 Official Info',
+    url: 'www.lovelive-anime.jp/hasunosora',
+    description: `
+:::tip
+蓮ノ空 Official Info feed.
+:::`,
+};
